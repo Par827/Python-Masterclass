a5dd0a94015342b180d932b171e90eacc9a626ed
BuildTools <unconfigured@null.spigotmc.org>
2021-11-23 13:39:28 -0600

    test commit number 2

--- ProgramFlow/searching.py
+++ ProgramFlow/searching.py
@@ -16,3 +16,20 @@ if found_at is not None:
     print("Item found at position {}".format(found_at))
 else:
     print("{} not found".format(item_to_find))
+
+# Modify the code inside this loop to stop when i is greater than zero and exactly divisible by 11
+for i in range(0, 100, 7):
+    print(i)
+    if i > 0 and i % 11 == 0:
+        break
+
+for i in range(1, 21):
+    print(i)
+    if i > 0 and i % 3 == 0 and i % 5 == 0:
+        continue
+
+# using continue
+for x in range(21):
+    if x % 3 == 0 or x % 5 == 0:
+        continue
+    print(x)
